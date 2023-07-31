fc20c31f68011b8236117c6de0018c1b5b155606
Marta Miras <martamirasmendez@gmail.com>
2023-07-31 16:20:28 +0200

    subo ejercicio 50

---
+++ es-50/exercise.js
@@ -0,0 +1,9 @@
+const names = ["Luca", "Marco", "Vittorio", "Giovanni"];
+// eliminar "Giovanni"
+const indexToRemove = names.indexOf("Giovanni");
+if (indexToRemove !== -1) {
+  names.splice(indexToRemove, 1);
+}
+names.push("Pippo");
+names.unshift("Giovanni");
+console.log(names); 
